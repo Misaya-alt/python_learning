c25b7125d0f59b2cdde164a4a420def2799c93eb
Misaya-alt <1171694840@qq.com>
2025-03-27 18:28:37 +0800

    第三天

--- learning/levle1_lesson4.3.py
+++ learning/levle1_lesson4.3.py
@@ -1,1 +1,18 @@
 # for循环
+"""for 临时变量 in 待处理数据集
+    循环满足条件时执行的代码"""
+
+name = "liuiqihao"
+for x in name:
+    # 将name中的内容依次取出赋值给临时变量x
+    # 然后在循环体中对x进行处理
+    print(x)
+
+count = 0
+name = "itheima is a brand of itcast"
+for i in name:
+    if i == "a":
+        count += 1
+    else:
+        count = count
+print(f"里面一个有{count}个a")
